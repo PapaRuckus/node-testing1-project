4ac01935e5661335b2201a1504a079369efcc9f3
Calvin K <calvinknaub@gmail.com>
2024-01-09 12:01:59 -0800

    Car

--- index.js
+++ index.js
@@ -115,7 +115,9 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0; // car initilizes with zero miles
     this.tank = tankSize; // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize;
+    this.mpg = mpg
+    this.name = name
   }
 
   /**
@@ -132,7 +134,16 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const maxDrive = this.tank * this.mpg
+    if (distance <= maxDrive) {
+      this.odometer += distance 
+      this.tank = this.tank - (distance / this.mpg)
+      return this.odometer
+    } else {
+      this.tank = 0
+      this.odometer += maxDrive
+    }
+    return this.odometer; 
   }
 
   /**
@@ -147,7 +158,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
